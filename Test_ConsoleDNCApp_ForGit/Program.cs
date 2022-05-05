59d092023e134ac50b83a80ee7a92257d6103df0
amitabh ninave <amitabh.ninave@gmail.com>
2022-05-05 15:29:43 +0530

    Initial commit

---
+++ Test_ConsoleDNCApp_ForGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_ConsoleDNCApp_ForGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is GIT demo.");
+        }
+    }
+}
